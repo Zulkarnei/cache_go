8c7165db7c577838cba1cec3d5d050d7307e52b1
Zhandos Zhalgasov <zhandos.zhalgasov.98@gmail.com>
2023-01-19 17:48:14 +0600

    release v2

--- cache/cache.go
+++ cache/cache.go
@@ -1,19 +1,73 @@
 package cache
 
-type Cache map[string]interface{}
+import (
+	"sync"
+	"time"
+)
 
-func New() Cache {
-	return make(map[string]interface{})
+type cacheItem struct {
+	value      interface{}
+	ttl        time.Duration
+	expireTime time.Time
 }
 
-func (c Cache) Set(key string, value interface{}) {
-	c[key] = value
+type Cache struct {
+	data   map[string]*cacheItem
+	mu     sync.RWMutex
+	delete chan string
 }
 
-func (c Cache) Get(key string) interface{} {
-	return c[key]
+func New() *Cache {
+	c := &Cache{
+		data:   make(map[string]*cacheItem),
+		delete: make(chan string),
+	}
+	go c.cleanup()
+	return c
 }
 
-func (c Cache) Delete(key string) {
-	delete(c, key)
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	expireTime := time.Now().Add(ttl)
+	c.data[key] = &cacheItem{value: value, ttl: ttl, expireTime: expireTime}
+}
+
+func (c *Cache) Get(key string) (interface{}, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	item, ok := c.data[key]
+	if ok {
+		if time.Now().After(item.expireTime) {
+			c.delete <- key
+			return nil, false
+		}
+		return item.value, true
+	}
+	return nil, false
+}
+
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.data, key)
+}
+
+func (c *Cache) cleanup() {
+	for {
+		select {
+		case key := <-c.delete:
+			c.mu.Lock()
+			delete(c.data, key)
+			c.mu.Unlock()
+		case <-time.After(time.Second * 30):
+			c.mu.Lock()
+			for key, item := range c.data {
+				if time.Now().After(item.expireTime) {
+					delete(c.data, key)
+				}
+			}
+			c.mu.Unlock()
+		}
+	}
 }
--- main.go
+++ main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"fmt"
 	"golang-ninja/basic/cache"
+	"time"
 )
 
 func main() {
-	cache := cache.New()
+	c := cache.New()
+	c.Set("key1", "value1", time.Second*5)
+	c.Set("key2", "value2", time.Second*10)
 
-	cache.Set("userId", 42)
-	userId := cache.Get("userId")
+	val, ok := c.Get("key1")
+	if ok {
+		fmt.Println("key1:", val)
+	} else {
+		fmt.Println("key1 not found")
+	}
 
-	fmt.Println(userId)
+	val, ok = c.Get("key2")
+	if ok {
+		fmt.Println("key2:", val)
+	} else {
+		fmt.Println("key2 not found")
+	}
 
-	cache.Delete("userId")
-	userId = cache.Get("userId")
+	// Wait for key1 to expire
+	time.Sleep(time.Second * 6)
 
-	fmt.Println(userId)
+	val, ok = c.Get("key1")
+	if ok {
+		fmt.Println("key1:", val)
+	} else {
+		fmt.Println("key1 not found")
+	}
 }
